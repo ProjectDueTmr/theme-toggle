471e66cbe7dd48404e51d274e1c5dd984f03105d
Nick Sanchez <nicksanchez2371@gmail.com>
2024-04-24 17:20:38 -0500

    rotation isnt working through js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const content = document.querySelector('.content')
 
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
-    const currentRotation = parseInt(getComputedStyle(content)
-.getPropertyValue('--rotation'))
+    const currentRotation = parseInt(getComputedStyle(content).getPropertyValue('--rotation'));
 content.style.setProperty('--rotation', currentRotation + 180);
 })
