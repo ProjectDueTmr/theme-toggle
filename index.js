75cd61eafec8131c132398e8992e6f24231f2ba1
Nick Sanchez <nicksanchez2371@gmail.com>
2024-04-26 19:52:27 -0500

    changed toggle button to be in a header

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const content = document.querySelector('.content')
-
+const content = document.querySelector('.content');
+const header = document.querySelector('.header');
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
+    header.classList.toggle('headerAlt')
     const currentRotation = parseInt(getComputedStyle(content).getPropertyValue('--rotation'));
 content.style.setProperty('--rotation', currentRotation + 180);
 })
